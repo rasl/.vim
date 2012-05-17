5a50417e1f686901924fffaac495da0fd2810298
Dmitriy Rassolov <dmitriy.rassolov@gmail.com>
2012-05-17 17:02:15 +0700

    ! move files to root

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 git submodule init
 git submodule update
-mv $HOME/.vimrc $HOME/.vimrc.backup
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.vimrc $HOME/.gvimrc
-mv $HOME/.vim $HOME/.vim.backup
-ln -s $PWD/.vim $HOME/.vim
+if [ "$PWD" -eq "$HOME/.vim" ]
+then
+	ln -s $PWD/.vimrc $HOME/.vimrc
+	ln -s $PWD/.vimrc $HOME/.gvimrc
+else
+	mv $HOME/.vimrc $HOME/.vimrc.backup
+	ln -s $PWD/.vimrc $HOME/.vimrc
+	ln -s $PWD/.vimrc $HOME/.gvimrc
+	mv $HOME/.vim $HOME/.vim.backup
+	ln -s $PWD $HOME/.vim
+fi
 ##
 # for DBGp-Remote-Debugger-Interface
 # (hard code in file debugger.vim, search debugger.py)
 ##
-if [ ! -d "$PWD/.vim/plugin" ]
+if [ ! -d "$PWD/plugin" ]
 then
-	mkdir -p $PWD/.vim/plugin
+	mkdir -p $PWD/plugin
 fi
-if [ ! -s $PWD/.vim/plugin/debugger.py ]
+if [ ! -s $PWD/plugin/debugger.py ]
 then	
-	ln -s ../../.vim/bundle/DBGp-Remote-Debugger-Interface/plugin/debugger.py $PWD/.vim/plugin/debugger.py
+	ln -s ../bundle/DBGp-Remote-Debugger-Interface/plugin/debugger.py $PWD/plugin/debugger.py
 fi
 vim +BundleInstall +qa!
 
